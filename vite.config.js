bb04b6f8adb4982fe5abe032e4f2ee44e1cd0aea
Pietro Boccia <pietro.boccia@avanade.com>
2025-03-31 18:03:13 +0200

    pushing dist

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,21 @@
 import { defineConfig } from 'vite';
+import { resolve } from 'path';
 
 export default defineConfig({
   base: '/cippoland/', // Replace 'cippoland' with your repository name
   build: {
     outDir: 'dist',
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          phaser: ['phaser'],
+        },
+      },
+    },
+  },
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, 'src'),
+    },
   },
 });
